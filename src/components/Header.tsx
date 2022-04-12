75deeda8c96ae3cbc239d0ced369736cd9e3d7de
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-04-12 10:31:08 +0900

    タイトルの大きさ変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {
 export const Header = (props: Props) => {
   return (
     <header className="navbar is-primary columns is-centered ">
-      <h2 className="is-size-1">{props.text}</h2>
+      <h1 className="is-size-1">{props.text}</h1>
     </header>
   );
 };
